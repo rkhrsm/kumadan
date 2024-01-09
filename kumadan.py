3f3f2ae458c46d7432d9e36af09f3be3f663ec3f
hayato <hayato.kawashima@gmail.com>
2024-01-09 13:23:44 +0900

    fix bag

--- kumadan.py
+++ kumadan.py
@@ -1,24 +1,25 @@
-from flask import Flask, flash, redirect, render_template, request, url_for,send_from_directory
+from openai import OpenAI
+from flask import Flask, flash, redirect, render_template, request, url_for, send_from_directory
 import threading
 from gtts import gTTS
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-from openai import OpenAI
 client = OpenAI(api_key="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")
 # message = "おなかが痛い"
 
-def encourage(message:str):
+
+def encourage(message: str):
     global encourage_msg
     prompt1 = [
         {"role": "user", "content": message+"。30字くらいで励ましてください"},
     ]
     res = client.chat.completions.create(
-        model = "gpt-3.5-turbo",  # GPTのエンジン名を指定します
-        messages = prompt1, # type: ignore
-        max_tokens = 200,  # 生成するトークンの最大数
-        n = 1,  # 生成するレスポンスの数
+        model="gpt-3.5-turbo",  # GPTのエンジン名を指定します
+        messages=prompt1,  # type: ignore
+        max_tokens=200,  # 生成するトークンの最大数
+        n=1,  # 生成するレスポンスの数
         temperature=0.5,  # 生成時のランダム性の制御
     )
     encourage_msg = res.choices[0].message.content
@@ -28,20 +29,20 @@ def encourage(message:str):
     #     print(f"content: {choice.message.content}")
 
 
-def get_number(message:str):
-    numbers = {"1":"#7119", "2":"#9110", "3":"188", "4":"177"}
+def get_number(message: str):
+    numbers = {"1": "#7119", "2": "#9110", "3": "188", "4": "177"}
     global number
     prompt2 = [
         {"role": "user", "content": message},
-        {"role": "user", "content":'今の相談が体や健康に関する相談の場合は"1"、'
-        '犯罪に関する相談の場合は"2"、消費者トラブルに関する相談の場合は"3"、'
-        '天気に関する相談の場合は"4"と答えよ。それ以外の場合は"0"と答えよ'},
+        {"role": "user", "content": '今の相談が体や健康に関する相談の場合は"1"、'
+         '犯罪に関する相談の場合は"2"、消費者トラブルに関する相談の場合は"3"、'
+         '天気に関する相談の場合は"4"と答えよ。それ以外の場合は"0"と答えよ'},
     ]
     res = client.chat.completions.create(
-        model = "gpt-3.5-turbo",  # GPTのエンジン名を指定します
-        messages = prompt2, # type: ignore
-        max_tokens = 20,  # 生成するトークンの最大数
-        n = 1,  # 生成するレスポンスの数
+        model="gpt-3.5-turbo",  # GPTのエンジン名を指定します
+        messages=prompt2,  # type: ignore
+        max_tokens=20,  # 生成するトークンの最大数
+        n=1,  # 生成するレスポンスの数
         temperature=1,  # 生成時のランダム性の制御
     )
     for i in res.choices:
@@ -52,19 +53,23 @@ def get_number(message:str):
             number = "0120279338"   # よりそいホットライン
         print(result)
 
+
 script = {
-    "#7119":"病気やケガに関する悩みは救急安心センター事業 #7119 に相談できます。",
-    "#9110":"犯罪や事故に関する悩みは警察相談専用電話 #9110 に相談できます。",
-    "188":"消費生活に関する悩みは消費者ホットライン 188 に相談できます。",
-    "177":"明後日までの天気は天気予報電話サービス 177 で聞くことができます。"
+    "#7119": "病気やケガに関する悩みは救急安心センター事業 #7119 に相談できます。",
+    "#9110": "犯罪や事故に関する悩みは警察相談専用電話 #9110 に相談できます。",
+    "188": "消費生活に関する悩みは消費者ホットライン 188 に相談できます。",
+    "177": "明後日までの天気は天気予報電話サービス 177 で聞くことができます。"
 }
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
         if request.form['input']:
-            t1 = threading.Thread(target=get_number, args=(request.form['input'],))
-            t2 = threading.Thread(target=encourage, args=(request.form['input'],))
+            t1 = threading.Thread(
+                target=get_number, args=(request.form['input'],))
+            t2 = threading.Thread(
+                target=encourage, args=(request.form['input'],))
             t1.start()
             t2.start()
             t1.join()
@@ -74,16 +79,19 @@ def main():
             else:
                 answer = "生活に関する悩みはよりそいホットラインに相談できます。"
             if number == "177":
+                global encourage_msg
                 encourage_msg = ""
-            tts1 = gTTS(text = encourage_msg + answer + number + 
+            tts1 = gTTS(text=encourage_msg + answer + number +
                         "に電話をかけますか？かける場合はボタンを押してください。", lang='ja', slow=True)
             tts1.save('sound/message.mp3')
             return render_template('main.html', msg=encourage_msg, number=number, script=answer)
     return render_template('main.html')
 
+
 @app.route('/tel')
 def tel():
     return render_template('tel.html')
+
 
 @app.route("/sound/<path:filename>")
 def play(filename):
